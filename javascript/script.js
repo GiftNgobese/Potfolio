552e3f441b49f53652633feb8e1d27ebeb95964f
GiftNgobese <136331355+GiftNgobese@users.noreply.github.com>
2023-12-25 14:17:19 +0200

    s

--- javascript/script.js
+++ javascript/script.js
@@ -7,3 +7,87 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         });
     });
 });
+
+
+
+
+
+
+
+// document.addEventListener('DOMContentLoaded', function () {
+//     var dropdownToggle = document.querySelector('.nav-link.dropdown-toggle');
+//     var dropdownMenu = document.querySelector('.dropdown-menu');
+//     var navbarToggler = document.querySelector('.navbar-toggler');
+//     var navbarMenu = document.querySelector('.collapse');
+
+//     // Dropdown toggle
+//     dropdownToggle.addEventListener('click', function (event) {
+//         event.stopPropagation();
+//         dropdownMenu.classList.toggle('hidden');
+//     });
+
+//     // Clicking outside the dropdown should close it
+//     window.addEventListener('click', function () {
+//         if (!dropdownMenu.classList.contains('hidden')) {
+//             dropdownMenu.classList.add('hidden');
+//         }
+//     });
+
+//     // Navbar toggler
+//     navbarToggler.addEventListener('click', function () {
+//         navbarMenu.classList.toggle('hidden');
+//     });
+// });
+
+
+
+// document.addEventListener('DOMContentLoaded', function () {
+//     var dropdownToggle = document.querySelector('.dropdown-toggle');
+//     var dropdownMenu = dropdownToggle.nextElementSibling;
+//     var navbarToggler = document.querySelector('.navbar-toggler');
+//     var navbarMenu = document.querySelector('.navbar-nav');
+    
+//     // Dropdown toggle
+//     dropdownToggle.addEventListener('click', function (event) {
+//         event.stopPropagation();
+//         dropdownMenu.classList.toggle('visible');
+//     });
+    
+//     // Clicking outside the dropdown should close it
+//     window.addEventListener('click', function () {
+//         if (dropdownMenu.classList.contains('visible')) {
+//             dropdownMenu.classList.remove('visible');
+//         }
+//     });
+    
+//     // Navbar toggler
+//     navbarToggler.addEventListener('click', function () {
+//         navbarMenu.classList.toggle('visible');
+//     });
+// });
+
+
+
+
+function toggleMenu() {
+    var navMenu = document.querySelector('.nav-menu');
+    navMenu.classList.toggle('active');
+}
+
+function toggleDropdown() {
+    var dropdownMenu = document.getElementById('dropdownMenu');
+    dropdownMenu.style.display = dropdownMenu.style.display === 'none' ? 'block' : 'none';
+}
+
+// Optional: Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+    if (!event.target.matches('.dropdown-toggle')) {
+        var dropdowns = document.getElementsByClassName("dropdown-menu");
+        for (var i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.style.display === 'block') {
+                openDropdown.style.display = 'none';
+            }
+        }
+    }
+}
